aae898a9a16a3b5528ad62a30e161c4834688dd6
Igor Serko <igor@ly.st>
2020-01-14 11:56:58 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup, find_packages
 from os import path
 
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
-# Python 3 only projects can skip this import
-from io import open
-
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
@@ -20,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(where="src"),
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    python_requires=">=3.7, <4",
     install_requires=["snowflake_connector_python", "environs"],
     entry_points={"console_scripts": ["query=snowflake_test.query:query"]},
 )
